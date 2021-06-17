bd15503774ba34b90ce73b65b36b1b45a753a45d
valentramos <ignacio1994.ir@gmail.com>
2021-06-17 00:07:14 -0400

    routes fonts & colors configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ColorsComponent } from './ui/colors/colors.component';
+import { FontsComponent } from './ui/fonts/fonts.component';
+
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/tipografia', pathMatch: 'full' },
+  { path: 'colores', component: ColorsComponent },
+  { path: 'tipografia', component: FontsComponent },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorsComponent } from './ui/colors/colors.component';
+import { FontsComponent } from './ui/fonts/fonts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorsComponent,
+    FontsComponent
   ],
   imports: [
     BrowserModule,
